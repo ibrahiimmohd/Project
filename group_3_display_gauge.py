be12d700f135e4f1061d19af2b95d6fbd712ce4d
Ibrahim Ali <47118191+ibrahiimmohd@users.noreply.github.com>
2020-12-01 14:07:36 -0500

    Added units

--- group_3_display_gauge.py
+++ group_3_display_gauge.py
@@ -15,17 +15,17 @@ class Example(Frame):
 
 
     def initUI(self):
-        self.master.title('Lab 9 and 10 A & B')
+        self.master.title('Lab 9/10 Temperature')
         self.pack(fill=BOTH, expand=1)
 
         canvas = Canvas(self)
         canvas.create_line(150, 200, 300, 200)    
 
-        canvas.create_text(25, 200, anchor=W, font='Purisa', 
+        canvas.create_text(25, 220, anchor=W, font='Purisa', 
         text='Enter Value: ')
         
         entry1 = tk.Entry()
-        canvas.create_window(75, 220, window=entry1)  
+        canvas.create_window(220, 220, window=entry1)  
         self.entryValue = entry1.get()
 
         x = lambda a : a * 100
@@ -47,16 +47,16 @@ class Example(Frame):
 
         button1 = tk.Button(text='Change Value',
         command= lambda : changeValue(entry1.get())) #TODO: Add this to make functionality work - , command=changeValue
-        canvas.create_window(200, 220, window=button1)
+        canvas.create_window(340, 220, window=button1)
 
         var = canvas.create_rectangle(200, 200, 250, self.recHeight,
         outline='#222', fill='#f76')
 
         canvas.create_line(150, 30, 150, 200)
-        canvas.create_text(140, 200, anchor=W, font='Purisa', text='0')
-        canvas.create_text(127, 150, anchor=W, font='Purisa', text='0.5')
-        canvas.create_text(140, 100, anchor=W, font='Purisa', text='1')
-        canvas.create_text(127, 50, anchor=W, font='Purisa', text='1.5')
+        canvas.create_text(30, 190, anchor=W, font='Purisa', text='Low     0°C')
+        canvas.create_text(30, 150, anchor=W, font='Purisa', text='Normal 0.5°C')
+        canvas.create_text(30, 100, anchor=W, font='Purisa', text='Normal 1°C')
+        canvas.create_text(30, 50, anchor=W, font='Purisa', text='High     1.5°C')
 
 
         canvas.pack(fill=BOTH, expand=1) 
