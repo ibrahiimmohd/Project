5dd1f7d32cf22414f23235e18715eeceb2e98813
Ibrahim Ali <47118191+ibrahiimmohd@users.noreply.github.com>
2020-12-04 00:17:42 -0500

    Refactored the codes

--- group_3_display_chart.py
+++ group_3_display_chart.py
@@ -7,6 +7,13 @@ import random
 class Example(Frame):
     entryValue = "0"
     recHeight = 300
+    rangeTag = ""
+    line1 = ""
+    line2 = ""
+    line3 = ""
+    line4 = ""
+    line5 = ""
+
     d1 = 0
     d2 = 0
     d3 = 0
@@ -37,7 +44,7 @@ class Example(Frame):
                 self.d6+=1
 
     def initUI(self):
-        self.master.title('Lab 9/10 Temperature')
+        self.master.title('Lab 11 Temperature')
         self.pack(fill=BOTH, expand=1)
 
         canvas = Canvas(self)
@@ -53,11 +60,10 @@ class Example(Frame):
         self.entryValue = entry1.get()
 
         def changeValue(text): 
-
+            
             high = int(text)
             low = high - 5
             rangeTxt = str('0' if low < 0 else low)+'-'+str(high)
-            canvas.create_text(155, 60, anchor=W, font='Purisa', text = rangeTxt)
 
             cal1 = (self.recHeight)-(int(self.d1) * 50)
             cal2 = (self.recHeight)-(int(self.d2) * 50)
@@ -66,18 +72,42 @@ class Example(Frame):
             cal5 = (self.recHeight)-(int(self.d5) * 50)
             cal6 = (self.recHeight)-(int(self.d6) * 50)
 
-            canvas.create_line(225, cal2 if self.d2 in range(low,high) else self.recHeight, 175, cal1 if self.d1 in range(low,high) else self.recHeight)
-            canvas.create_line(275, cal3 if self.d3 in range(low,high) else self.recHeight, 225, cal2 if self.d2 in range(low,high) else self.recHeight)
-            canvas.create_line(325, cal4 if self.d4 in range(low,high) else self.recHeight, 275, cal3 if self.d3 in range(low,high) else self.recHeight)
-            canvas.create_line(375, cal5 if self.d5 in range(low,high) else self.recHeight, 325, cal4 if self.d4 in range(low,high) else self.recHeight)
-            canvas.create_line(425, cal6 if self.d6 in range(low,high) else self.recHeight, 375, cal5 if self.d5 in range(low,high) else self.recHeight)
+            check1 = cal1 if self.d1 in range(low,high) else self.recHeight
+            check2 = cal2 if self.d2 in range(low,high) else self.recHeight
+            check3 = cal3 if self.d3 in range(low,high) else self.recHeight
+            check4 = cal4 if self.d4 in range(low,high) else self.recHeight
+            check5 = cal5 if self.d5 in range(low,high) else self.recHeight
+            check6 = cal6 if self.d6 in range(low,high) else self.recHeight
 
-            canvas.coords(var1, 150, 300, 200, cal1 if self.d1 in range(low,high) else self.recHeight)
-            canvas.coords(var2, 200, 300, 250, cal2 if self.d2 in range(low,high) else self.recHeight)
-            canvas.coords(var3, 250, 300, 300, cal3 if self.d3 in range(low,high) else self.recHeight)
-            canvas.coords(var4, 300, 300, 350, cal4 if self.d4 in range(low,high) else self.recHeight)
-            canvas.coords(var5, 350, 300, 400, cal5 if self.d5 in range(low,high) else self.recHeight)
-            canvas.coords(var6, 400, 300, 450, cal6 if self.d6 in range(low,high) else self.recHeight)
+            if(self.rangeTag == ''):
+                self.rangeTag = canvas.create_text(155, 60, anchor=W, font='Purisa', text = rangeTxt, tag="rangeTag")
+                self.line1 = canvas.create_line(225, check2, 175, check1)
+                self.line2 = canvas.create_line(275, check3, 225, check2)
+                self.line3 = canvas.create_line(325, check4, 275, check3)
+                self.line4 = canvas.create_line(375, check5, 325, check4)
+                self.line5 = canvas.create_line(425, check6, 375, check5)
+            else:
+                canvas.delete(self.rangeTag)
+                canvas.delete(self.line1)
+                canvas.delete(self.line2)
+                canvas.delete(self.line3)
+                canvas.delete(self.line4)
+                canvas.delete(self.line5)
+
+                self.rangeTag = canvas.create_text(155, 60, anchor=W, font='Purisa', text = rangeTxt, tag="rangeTag")
+                self.line1 = canvas.create_line(225, check2, 175, check1)
+                self.line2 = canvas.create_line(275, check3, 225, check2)
+                self.line3 = canvas.create_line(325, check4, 275, check3)
+                self.line4 = canvas.create_line(375, check5, 325, check4)
+                self.line5 = canvas.create_line(425, check6, 375, check5)
+
+
+            canvas.coords(var1, 150, 300, 200, check1)
+            canvas.coords(var2, 200, 300, 250, check2)
+            canvas.coords(var3, 250, 300, 300, check3)
+            canvas.coords(var4, 300, 300, 350, check4)
+            canvas.coords(var5, 350, 300, 400, check5)
+            canvas.coords(var6, 400, 300, 450, check6)
  
         button1 = tk.Button(text='Go', width=10,
         command= lambda : changeValue(entry1.get()))
